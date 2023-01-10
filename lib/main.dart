d1e1bff162051c4e2a0b6d5328a0b7b63bc3535d
UNKNOWN74056 <hamza74056@gmail.com>
2023-01-10 23:17:13 +0500

    podari changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:finalyear/splashscreen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(const MyApp());
 }
 
--- lib/pages/signuppage.dart
+++ lib/pages/signuppage.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:finalyear/pages/loginpage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
@@ -13,11 +15,42 @@ class signuppage extends StatefulWidget {
 final _formKey = GlobalKey<FormState>();
 
 class _signuppageState extends State<signuppage> {
-  final profession = ['Coache', 'Player'];
-  //String? selectedval = 'Coache';
+  //controllers for text fields
+  final _firstnamecontroller = TextEditingController();
+  final _lastnamecontroller = TextEditingController();
+  final _professioncontroller = TextEditingController();
+  final _gendercontroller = TextEditingController();
+  final _sportcontroller = TextEditingController();
+  final _emailcontroller = TextEditingController();
+  final _passwordcontroller = TextEditingController();
+  final _confirmpasswordcontroller = TextEditingController();
+  final _phonenumbercontroller = TextEditingController();
 
+  // this is the dispose funtion to dispose the data
+  @override
+  void dispose() {
+    _firstnamecontroller.dispose();
+    _lastnamecontroller.dispose();
+    _professioncontroller.dispose();
+    _gendercontroller.dispose();
+    _sportcontroller.dispose();
+    _emailcontroller.dispose();
+    _passwordcontroller.dispose();
+    _confirmpasswordcontroller.dispose();
+    _phonenumbercontroller.dispose();
+
+    super.dispose();
+  }
+
+  //options for profession
+  final profession = ['Coache', 'Player'];
+  String? selectedval = 'coache';
+
+  //options for gender
   final gender = ['Male', 'Female', 'Other'];
-  //String? selected = 'Male';
+  String? selected = 'Male';
+
+  //options for type of sports
   final sports = [
     'FootBall',
     'BasketBall',
@@ -30,6 +63,64 @@ class _signuppageState extends State<signuppage> {
     'Tent pagging',
     'Gymnastic'
   ];
+  String? selected1 = 'FootBall';
+
+  //function to create user
+  Future signup() async {
+    if (passwrodconfirm()) {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailcontroller.text.trim(),
+        password: _passwordcontroller.text.trim(),
+      );
+
+      Adduserdeatial(
+        _firstnamecontroller.text.trim(),
+        _lastnamecontroller.text.trim(),
+        _professioncontroller.text.trim(),
+        _gendercontroller.text.trim(),
+        _sportcontroller.text.trim(),
+        _emailcontroller.text.trim(),
+        _passwordcontroller.text.trim(),
+        _confirmpasswordcontroller.text.trim(),
+        _phonenumbercontroller.text.trim(),
+      );
+    }
+  }
+
+  //add user details to firestore database
+  Future Adduserdeatial(
+      String firstname,
+      String lastname,
+      String profession,
+      String gender,
+      String sports,
+      String email,
+      String password,
+      String confirm,
+      String phone) async {
+    await FirebaseFirestore.instance.collection('users').add({
+      'firstname': firstname,
+      'lastname': lastname,
+      'profession': profession,
+      'gender': gender,
+      'sport': sports,
+      'email': email,
+      'password': password,
+      'confirmpassword': confirm,
+      'phoneNumber': phone,
+    });
+  }
+
+  //bool function to confirm password
+  bool passwrodconfirm() {
+    if (_passwordcontroller.text.trim() ==
+        _confirmpasswordcontroller.text.trim()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -48,6 +139,13 @@ class _signuppageState extends State<signuppage> {
           ),
         ),
         const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          "Register below with your details",
+          style: TextStyle(fontSize: 20),
+        ),
+        const SizedBox(
           height: 25,
         ),
         Form(
@@ -57,6 +155,7 @@ class _signuppageState extends State<signuppage> {
             child: Column(
               children: [
                 TextFormField(
+                    controller: _firstnamecontroller,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20)),
@@ -73,6 +172,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 TextFormField(
+                    controller: _lastnamecontroller,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20)),
@@ -89,8 +189,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 DropdownButtonFormField(
-
-                    //value: selectedval,
+                   // value: selectedval,
                     items: profession
                         .map(
                           (e) => DropdownMenuItem(
@@ -101,7 +200,7 @@ class _signuppageState extends State<signuppage> {
                         .toList(),
                     onChanged: (val) {
                       setState(() {
-                        //  selectedval = val as String;
+                        selectedval = val as String;
                       });
                     },
                     icon: const Icon(
@@ -110,6 +209,7 @@ class _signuppageState extends State<signuppage> {
                     ),
                     decoration: InputDecoration(
                         labelText: "Your profession",
+                        hintText: "select in option",
                         prefixIcon: const Icon(Icons.sports),
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20))),
@@ -124,7 +224,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 DropdownButtonFormField(
-                    //value: selected,
+                   // value: selected,
                     items: gender
                         .map(
                           (e) => DropdownMenuItem(
@@ -135,7 +235,7 @@ class _signuppageState extends State<signuppage> {
                         .toList(),
                     onChanged: (val) {
                       setState(() {
-                        //  selected = val as String;
+                        selected = val as String;
                       });
                     },
                     icon: const Icon(
@@ -144,6 +244,7 @@ class _signuppageState extends State<signuppage> {
                     ),
                     decoration: InputDecoration(
                         labelText: "Your gender",
+                        hintText: "select option",
                         prefixIcon: const Icon(Icons.sports),
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20))),
@@ -158,7 +259,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 DropdownButtonFormField(
-                    //value: selected,
+                    value: selected1,
                     items: sports
                         .map(
                           (e) => DropdownMenuItem(
@@ -169,7 +270,7 @@ class _signuppageState extends State<signuppage> {
                         .toList(),
                     onChanged: (val) {
                       setState(() {
-                        //  selected = val as String;
+                        selected1 = val as String;
                       });
                     },
                     icon: const Icon(
@@ -178,6 +279,7 @@ class _signuppageState extends State<signuppage> {
                     ),
                     decoration: InputDecoration(
                         labelText: "Your sports",
+                        hintText: "select option",
                         prefixIcon: const Icon(Icons.sports),
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20))),
@@ -192,6 +294,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 TextFormField(
+                    controller: _emailcontroller,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20)),
@@ -208,6 +311,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 TextFormField(
+                    controller: _passwordcontroller,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20)),
@@ -224,6 +328,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 TextFormField(
+                    controller: _confirmpasswordcontroller,
                     decoration: InputDecoration(
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20)),
@@ -241,6 +346,7 @@ class _signuppageState extends State<signuppage> {
                   height: 25,
                 ),
                 InternationalPhoneNumberInput(
+                    textFieldController: _phonenumbercontroller,
                     onInputChanged: (val) {},
                     inputDecoration: const InputDecoration(
                       border: InputBorder.none,
@@ -261,19 +367,21 @@ class _signuppageState extends State<signuppage> {
                   child: Padding(
                     padding: const EdgeInsets.only(left: 30, right: 30),
                     child: ElevatedButton(
+                        //  onPressed: () {
+
+                        //   if (_formKey.currentState!.validate()) {
+                        //     ScaffoldMessenger.of(context).showSnackBar(
+                        //       const SnackBar(content: Text('Processing Data')),
+                        //     );
+                        //   }
+                        //},
                         onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Processing Data')),
-                            );
-                          }
+                          // i add here get for pop bottom
+                          signup();
+                          Get.snackbar("Sing up", "Successfully",
+                              duration: const Duration(seconds: 1),
+                              snackPosition: SnackPosition.BOTTOM);
                         },
-                        // onPressed: () {
-                        //   // i add here get for pop bottom
-                        //   Get.snackbar("Sing up", "Successfully",
-                        //       duration: const Duration(seconds: 1),
-                        //       snackPosition: SnackPosition.BOTTOM);
-                        // },
                         child: const Text("SIGIN UP")),
                   ),
                 ),
