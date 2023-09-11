ccc78a7dfd52828ea529ef12eee43e8a740d6cde
UNKNOWN74056 <hamza74056@gmail.com>
2023-09-11 06:45:29 +0500

    changes

--- lib/AS A PLAYER/internationalsports/tournmaents tabs/footballtour.dart
+++ lib/AS A PLAYER/internationalsports/tournmaents tabs/footballtour.dart
@@ -1,8 +1,8 @@
+import 'package:finalyear/API/footballApi/soccer.dart';
 import 'package:finalyear/GETX/tournamentdata.dart';
+import 'package:finalyear/wedgets/data/Tournament-Data.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import '../../../API/footballApi/soccer.dart';
 
 class footballtour extends StatefulWidget {
   const footballtour({super.key});
@@ -30,130 +30,19 @@ class _footballtourState extends State<footballtour> {
             () => ListView(
               children: controller.tournamentlist
                   .where((e) => e.tournamentsport == 'football')
-                  .map(
-                    (element) => Column(
-                      children: [
-                        const SizedBox(height: 10),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: GestureDetector(
-                            onTap: () {
-                              Get.to(SoccerApp());
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.grey.shade300,
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              height: 210,
-                              width: MediaQuery.of(context).size.width,
-                              child: Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        CircleAvatar(
-                                          radius: 25,
-                                          backgroundImage: NetworkImage(
-                                            element.tournamentimage,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              element.tournamentname,
-                                              style: const TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            Text(
-                                              element.tournamentlocation,
-                                              style: const TextStyle(
-                                                color: Colors.grey,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 20),
-                                    Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.schedule,
-                                          color: Colors.blue,
-                                        ),
-                                        const SizedBox(width: 5),
-                                        const Text(
-                                          'Start At:',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text(
-                                          element.startdate,
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.schedule,
-                                          color: Colors.red,
-                                        ),
-                                        const SizedBox(width: 5),
-                                        const Text(
-                                          'End At:',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text(element.enddate),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Row(
-                                      children: [
-                                        const Icon(
-                                          Icons.attach_money,
-                                          color: Colors.green,
-                                        ),
-                                        const SizedBox(width: 5),
-                                        const Text(
-                                          'Price Pool:',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        const SizedBox(width: 5),
-                                        Text(
-                                          element.price,
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16,
-                                            color: Colors.green,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
+                  .map((element) => GestureDetector(
+                        onTap: () {
+                          Get.to(() => SoccerApp());
+                        },
+                        child: TournamentCard(
+                          tournamentName: element.tournamentname,
+                          tournamentlocation: element.tournamentlocation,
+                          startDate: element.startdate,
+                          endDate: element.enddate,
+                          price: element.price,
+                          tournamentimage: element.tournamentimage,
                         ),
-                      ],
-                    ),
-                  )
+                      ))
                   .toList(),
             ),
           ),
---
+++ lib/wedgets/data/Tournament-Data.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class TournamentCard extends StatefulWidget {
+  final dynamic tournamentName;
+  final dynamic tournamentlocation;
+  final dynamic startDate;
+  final dynamic endDate;
+  final dynamic price;
+  final dynamic tournamentimage;
+  const TournamentCard({
+    super.key,
+    required this.tournamentName,
+    required this.tournamentlocation,
+    required this.startDate,
+    required this.endDate,
+    required this.price,
+    required this.tournamentimage,
+  });
+
+  @override
+  State<TournamentCard> createState() => _TournamentCardState();
+}
+
+class _TournamentCardState extends State<TournamentCard> {
+  bool isLoading = true; // Initially set to true for shimmer loading
+
+  @override
+  void initState() {
+    super.initState();
+    // Simulate data loading delay
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        isLoading = false; // Set to false when data is loaded
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading) {
+      return ListView.builder(
+          itemCount: 5,
+          shrinkWrap: true,
+          itemBuilder: (context, i) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              child: Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  height: 150,
+                  width: MediaQuery.of(context).size.width,
+                  child: const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey),
+                                ),
+                                Text(
+                                  "",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [],
+                        ),
+                        Row(
+                          children: [],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          });
+    }
+    //original ui
+    else {
+      return GestureDetector(
+        child: Column(children: [
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              height: 210,
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 25,
+                          backgroundImage: NetworkImage(
+                            widget.tournamentimage,
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.tournamentName,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              widget.tournamentlocation,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.schedule,
+                          color: Colors.blue,
+                        ),
+                        const SizedBox(width: 5),
+                        const Text(
+                          'Start At:',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          widget.startDate,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.schedule,
+                          color: Colors.red,
+                        ),
+                        const SizedBox(width: 5),
+                        const Text(
+                          'End At:',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(widget.endDate),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.attach_money,
+                          color: Colors.green,
+                        ),
+                        const SizedBox(width: 5),
+                        const Text(
+                          'Price Pool:',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        Text(
+                          widget.price,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ]),
+      );
+    }
+  }
+}
